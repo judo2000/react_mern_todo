18b77482fee2da066600afbcc6abce37bf244f7d
Stephen Moore <judo2000@comcast.net>
2023-09-10 19:51:01 -0600

    Add app.use express.json and express.urlecoded middleware to index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,8 +2,13 @@ import express from "express";
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("<h1>Hello World</h1>");
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.post("/", (req, res) => {
+  // need data to create new note/todo
+  console.log(req.body);
+  res.json({ message: "I am listening" });
 });
 
 app.listen(8000, () => {
