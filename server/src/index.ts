1d572338ce264be57d5c26f23fcc6d40c5669d2b
Stephen Moore <judo2000@comcast.net>
2023-09-10 19:37:53 -0600

    Install ts-node-dev package, add dev scripit to package.json, move index.ts to src folder and update tsconfig.json

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,7 +3,7 @@ import express from "express";
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("<h1>Hello World!</h1>");
+  res.send("<h1>Hello World</h1>");
 });
 
 app.listen(8000, () => {
