d0c339bddef034d3c1a484207890a69103fa8d34
Stephen Moore <judo2000@comcast.net>
2023-09-19 17:40:52 -0600

    Fix CORS error to submit note

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,12 @@
 import express from "express";
 import "./db";
+import cors from "cors";
+
 import noteRouter from "./routers/note";
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
