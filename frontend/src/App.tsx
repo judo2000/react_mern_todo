2825371f2d9ee63f5ba8e8fa7285a3c86f789f33
Stephen Moore <judo2000@comcast.net>
2023-09-19 17:34:54 -0600

    Install axios and create submit function

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import React, { useState, ChangeEventHandler } from "react";
 import NoteItem from "./components/NoteItem";
+import axios from "axios";
 
 const App = () => {
   // const [title, setTitle] = useState("");
@@ -19,7 +20,20 @@ const App = () => {
 
   return (
     <div className="max-w-3xl mx-auto space-y-6">
-      <div className="bg-white shadow-lg rounded p-5 space-y-6">
+      <form
+        onSubmit={async (e) => {
+          e.preventDefault();
+          const { data } = await axios.post(
+            "http://localhost:8000/note/create",
+            {
+              title: values.title,
+              description: values.description,
+            }
+          );
+          console.log(data);
+        }}
+        className="bg-white shadow-lg rounded p-5 space-y-6"
+      >
         <h1 className="font-semibold text-2xl text-center">Note Application</h1>
         <div>
           <input
@@ -41,16 +55,11 @@ const App = () => {
           />
         </div>
         <div className="text-center">
-          <button
-            className="bg-blue-500 text-white px-5 py-2 rounded"
-            onClick={() => {
-              console.log(values);
-            }}
-          >
+          <button className="bg-blue-500 text-white px-5 py-2 rounded">
             Submit
           </button>
         </div>
-      </div>
+      </form>
       {/* Note Items*/}
 
       <NoteItem title="My first reusable component" />
