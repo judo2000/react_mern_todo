1412748ee6367dd2f7ff19a0fa5c099e910d1b1b
Stephen Moore <judo2000@comcast.net>
2023-09-19 17:24:57 -0600

    Reconfigure useState to use handleChange function to set title and description

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,34 +1,52 @@
-import React, { useState } from "react";
+import React, { useState, ChangeEventHandler } from "react";
 import NoteItem from "./components/NoteItem";
 
 const App = () => {
-  const [title, setTitle] = useState("");
+  // const [title, setTitle] = useState("");
+  // const [description, setDescription] = useState("");
+
+  const [values, setValues] = useState({
+    title: "",
+    description: "",
+  });
+
+  const handleChange: ChangeEventHandler<
+    HTMLInputElement | HTMLTextAreaElement
+  > = ({ target }) => {
+    const { name, value } = target;
+    setValues({ ...values, [name]: value });
+  };
 
   return (
     <div className="max-w-3xl mx-auto space-y-6">
       <div className="bg-white shadow-lg rounded p-5 space-y-6">
         <h1 className="font-semibold text-2xl text-center">Note Application</h1>
         <div>
-          {title.trim() && title.length < 3 ? (
-            <p className="text-red-500">Title is too short</p>
-          ) : null}
           <input
             type="text"
             className="w-full border-b-2 border-gray-700 outline-none"
             placeholder="Title"
-            onChange={(e) => {
-              setTitle(e.target.value);
-            }}
+            name="title"
+            value={values.title}
+            onChange={handleChange}
           />
         </div>
         <div>
           <textarea
             className="w-full border-b-2 border-gray-700 outline-none resize-none h-36"
             placeholder="Description"
+            name="description"
+            value={values.description}
+            onChange={handleChange}
           />
         </div>
         <div className="text-center">
-          <button className="bg-blue-500 text-white px-5 py-2 rounded">
+          <button
+            className="bg-blue-500 text-white px-5 py-2 rounded"
+            onClick={() => {
+              console.log(values);
+            }}
+          >
             Submit
           </button>
         </div>
